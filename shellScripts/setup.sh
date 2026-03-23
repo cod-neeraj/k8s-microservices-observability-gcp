40167cc4ea3fdbb25c80413f75c9f530469b7acb
neeraj <neerajmittal957@gmail.com>
2026-03-23 21:52:36 +0530

    ff

---
+++ shellScripts/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Istio..."
+
+curl -L https://istio.io/downloadIstio | sh -
+
+ISTIO_VERSION=$(ls -d istio-* | head -n 1)
+cd $ISTIO_VERSION
+
+export PATH=$PWD/bin:$PATH
+
+istioctl install --set profile=demo -y
+
+kubectl label namespace default istio-injection=enabled --overwrite
+
+echo "Installing Istio observability addons..."
+
+kubectl apply -f samples/addons/prometheus.yaml
+kubectl apply -f samples/addons/grafana.yaml
+kubectl apply -f samples/addons/kiali.yaml
+kubectl apply -f samples/addons/jaeger.yaml
+
+echo "Setup completed!"#!/bin/bash
+cd ..
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+helm repo update
+
+kubectl create namespace monitoring
+
+helm install monitoring prometheus-community/kube-prometheus-stack \
+-n monitoring
